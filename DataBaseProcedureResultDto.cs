0e5c3abc83310987019993b05d7684755ca691e0
Pedro Tersetti Freidinger <pedrotersetti3@gmail.com>
2024-03-04 23:44:01 -0300

    Refactor some code

---
+++ DataBaseProcedureResultDto.cs
@@ -0,0 +1,4 @@
+﻿using PedroTer7.Rinha2024Q1.Database.Enums;
+
+namespace PedroTer7.Rinha2024Q1.Database.Dtos;
+public record class DataBaseProcedureResultDto(DataBaseProcedureResultCodeEnum ResultCode);
---
+++ GetAccountStatementProcedureResultDto.cs
@@ -0,0 +1,8 @@
+﻿using PedroTer7.Rinha2024Q1.Database.Enums;
+using PedroTer7.Rinha2024Q1.Database.Models;
+
+namespace PedroTer7.Rinha2024Q1.Database.Dtos;
+
+public record class GetAccountStatementProcedureResultDto(DataBaseProcedureResultCodeEnum OperationResult, 
+    int AccountBalance, int AccountLimit, DateTime StatementUtcTimestamp, IEnumerable<AccountTransactionLogModel> TransactionHistory) 
+        : DataBaseProcedureResultDto(OperationResult);
